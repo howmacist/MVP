e169ad8b9e5b8e4c871375b020883f728e3a5b80
Matt Riddoch <matt.riddoch@gmail.com>
2014-07-17 13:41:40 -0600

    bring in pod files

---
+++ flows/Pods/Pods-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// GDIIndexBar
+#define COCOAPODS_POD_AVAILABLE_GDIIndexBar
+#define COCOAPODS_VERSION_MAJOR_GDIIndexBar 0
+#define COCOAPODS_VERSION_MINOR_GDIIndexBar 3
+#define COCOAPODS_VERSION_PATCH_GDIIndexBar 2
+
+// JASidePanels
+#define COCOAPODS_POD_AVAILABLE_JASidePanels
+#define COCOAPODS_VERSION_MAJOR_JASidePanels 1
+#define COCOAPODS_VERSION_MINOR_JASidePanels 3
+#define COCOAPODS_VERSION_PATCH_JASidePanels 2
+
+// JSONModel
+#define COCOAPODS_POD_AVAILABLE_JSONModel
+#define COCOAPODS_VERSION_MAJOR_JSONModel 0
+#define COCOAPODS_VERSION_MINOR_JSONModel 13
+#define COCOAPODS_VERSION_PATCH_JSONModel 0
+
+// TYMActivityIndicatorView
+#define COCOAPODS_POD_AVAILABLE_TYMActivityIndicatorView
+#define COCOAPODS_VERSION_MAJOR_TYMActivityIndicatorView 0
+#define COCOAPODS_VERSION_MINOR_TYMActivityIndicatorView 2
+#define COCOAPODS_VERSION_PATCH_TYMActivityIndicatorView 0
+
---
+++ flows/Pods/TYMActivityIndicatorView/README.markdown
@@ -0,0 +1,29 @@
+# TYMActivityIndicatorView
+
+A simple activity indicator view. It uses Core Animation to handle all the animation related stuffs.
+
+TYMActivityIndicatorView is tested on iOS 7 and requires ARC.
+
+![Screenshot](https://raw2.github.com/krafttuc/TYMActivityIndicatorView/master/Screenshot.png)
+
+
+## Features
+
+* Simple APIs which are much like those in the `UIActivityIndicatorView` class.
+* You're able to customize the appearance and behaviors through [appearnce proxy](http://nshipster.com/uiappearance/).
+* You may set the progress (rotation angle).
+
+
+## Installation
+
+Simply add the files in the `TYMActivityIndicatorView` directory to your project.
+
+
+## Example
+
+Open up the included Xcode project for an example app. See [TYMActivityIndicatorView.h](https://github.com/krafttuc/TYMActivityIndicatorView/blob/master/TYMActivityIndicatorView/TYMActivityIndicatorView.h) for full documentation.
+
+
+## License
+
+Released under the MIT license.
